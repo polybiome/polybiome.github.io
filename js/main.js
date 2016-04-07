1c9ec6fe4a9992008acc4285d216aab12002bb99
MattBeepMac <dtcdvd@gmail.com>
2016-04-07 20:57:42 +0200

    'asdf'

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// $(".navbar-nav a").mouseover(function(){
+// 	$('.navbar-nav>.active>a').css({
+// 		'box-shadow' : 'none'
+// 	});
+// });
+// $(".navbar-nav a").mouseout(function(){
+// 	$('.navbar-nav>.active>a').css({
+// 		'box-shadow' : 'inset 0 3px 9px rgba(0,0,0,0.2)'
+// 	});
+// });
+$(window).scroll(function(){
+	var topScroll = $(this).scrollTop();
+	if (topScroll >= $('.main').height()){
+		$(".navbar-default .navbar-collapse").addClass('navbarEffect');
+	} else{
+		$(".navbar-default .navbar-collapse").removeClass('navbarEffect');
+	};
+	if (topScroll >=  $(window).height() - 0.5*$('#ribbon').height()){
+		$('#ribbon').css({
+			'transform' : 'translate(0px, '+ 0 +'px)'
+		});
+	};
+
+	if(topScroll > $('.food').offset().top - ($(window).height() / 1.2)) {
+		$('.food').each(function(i){
+		  setTimeout(function(){
+		    $('.food').eq(i).addClass('is-showing');
+		  }, (700 * (Math.exp(i * 0.14))) - 700);
+		});
+	}
+
+	if (topScroll >=  $(window).height() + 7*$('#mol1').height()){
+		$('#mol1').css({
+			'transform' : 'translate(0px, 0px)'
+		});
+		$('#mol2').css({
+			'transform' : 'translate(0px, 0px)'
+		});
+	};
+	if (topScroll >=  $(window).height() + 24*$('#logoIgem').height()){
+		$('#logoIgem').css({
+			'transform' : 'translate(0px, 0px)'
+		});
+		$('#logoDracBlue').css({
+			'transform' : 'translate(0px, 0px)'
+		});
+	};
+})
